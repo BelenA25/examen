d9219648373303b38b191638ea3a4d4d213cd4d7
Belen <beckiar23@gmail.com>
2023-06-01 09:23:20 -0400

    Prueba de interfaz de usuario: deberia mostrar el cambio correspondiente

--- cypress/e2e/sumador.cy.js
+++ cypress/e2e/sumador.cy.js
@@ -4,4 +4,10 @@ describe("Ejercicio 1", () => {
     cy.get("label").should("contain", "Monto venta:");
     cy.get("label").should("contain", "Efectivo:");
   });
+  it("Deberia mostrar el cambio correspondiente al ingresar el monto de venta y el efectivo", () => {
+    cy.visit("/");
+    cy.get("#monto-venta").type(25);
+    cy.get("#efectivo").type(50);
+    cy.get("#resultado-div").should("contain",25);
+  });
 });
--- src/presenter.js
+++ src/presenter.js
@@ -1,15 +1,17 @@
 import sumar from "./sumador";
 
-const first = document.querySelector("#primer-numero");
-const second = document.querySelector("#segundo-numero");
-const form = document.querySelector("#sumar-form");
+const montoVenta = document.querySelector("#monto-venta");
+const efectivo = document.querySelector("#efectivo");
+const form = document.querySelector("#venta-form");
 const div = document.querySelector("#resultado-div");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
-  const firstNumber = Number.parseInt(first.value);
-  const secondNumber = Number.parseInt(second.value);
+  const montoVenta = Number.parseInt(first.value);
+  const efectivo = Number.parseInt(second.value);
 
-  div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+  let cambio = efectivo - cambio;
+
+  div.innerHTML = "<p>" + cambio + "</p>";
 });
